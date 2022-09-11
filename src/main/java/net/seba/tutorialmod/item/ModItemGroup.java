00c947f74cbacd18f3dda184b1abb0e818b69666
Sebastian-Cahuana <sebastian.666.beraun@gmail.com>
2022-09-11 12:15:31 -0300

    first change

--- src/main/java/net/seba/tutorialmod/item/ModItemGroup.java
+++ src/main/java/net/seba/tutorialmod/item/ModItemGroup.java
@@ -5,7 +5,7 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.seba.tutorialmod.TutorialMod;
-
+/*Aqui se crea un grupo de items (nueva columna dentro del inventario)*/
 public class ModItemGroup {
     public static final ItemGroup TANZANITE = FabricItemGroupBuilder.build(
             new Identifier(TutorialMod.MOD_ID,"tanzanite"),()-> new ItemStack(ModItems.TANZANITE));
--- src/main/java/net/seba/tutorialmod/item/ModItems.java
+++ src/main/java/net/seba/tutorialmod/item/ModItems.java
@@ -2,7 +2,7 @@ package net.seba.tutorialmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-//import net.minecraft.item.ItemGroup;
+//import net.minecraft.item.ItemGroup; //poner o dejarlo como comentario dependiendo del group
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.seba.tutorialmod.TutorialMod;
@@ -14,11 +14,18 @@ public class ModItems {
     private static Item registerItem(String name, Item item){
         return Registry.register(Registry.ITEM,new Identifier(TutorialMod.MOD_ID,name),item);
     }
+    /*El en el caso 1 se agrega un item "raw_tanzanite" al grupo de MISC (miscelaneos)
+    En el caso 2 se agrega un item al grupo creado por mi en ModItemGroup
+    */
     //No uppercase
+    //1)
     //public static final Item RAW_TANZANITE = registerItem("raw_tanzanite", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    //2)
     public static final Item RAW_TANZANITE = registerItem("raw_tanzanite", new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE)));
 
     public static final Item TANZANITE = registerItem("tanzanite", new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE)));
 
+    //objeto nuevo
+    public static final Item BLACKAMONGUS = registerItem("black_amongus",new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE)));
 
 }
